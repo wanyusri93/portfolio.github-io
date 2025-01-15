896db5e4665effc5bda68478caea035d895b3dde
WAN MUHAMMAD YUSR BIN WAN KAMARUDIN <82821808+wanyusri93@users.noreply.github.com>
2025-01-15 08:06:50 +0800

    Add files via upload

---
+++ dist/includes/db.php
@@ -0,0 +1,14 @@
+<?php
+
+  $server = "";
+  $username = "";
+  $password = "";
+  $dbname = "";
+
+  $conn = mysqli_connect($server, $username, $password, $dbname);
+
+  if(!$conn){
+die("connection Failed:" .mysqli_connect_error());
+
+  }
+  ?>
